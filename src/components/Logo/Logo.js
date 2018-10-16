ec5f902edfd708bb2b46705fac4194f8c2597a09
Guilherme Andrade <guilhermedval@hotmail.com>
2018-10-16 16:49:31 -0300

    v1.0 - frontend smartbrain app

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+		<Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 125, width: 125 }} >
+			<div className="Tilt-inner pa3"> 
+				<img src={brain} alt="brain logo"  />	 
+			</div>
+		</Tilt>
+		</div>
+	);
+}
+
+export default Logo;
